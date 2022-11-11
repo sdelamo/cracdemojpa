36ac15ad7496a8f212e6f049307fbefc6e4e1ad4
Sergio del Amo <sergio.delamo@softamo.com>
2022-11-11 17:19:15 +0100

    H2console as a resources

--- src/main/java/datajpah2/dev/H2Console.java
+++ src/main/java/datajpah2/dev/H2Console.java
@@ -4,7 +4,11 @@ import io.micronaut.context.annotation.Requires;
 import io.micronaut.context.env.Environment;
 import io.micronaut.context.event.ApplicationEventListener;
 import io.micronaut.context.event.StartupEvent;
+import io.micronaut.context.exceptions.ConfigurationException;
+import io.micronaut.crac.OrderedResource;
 import jakarta.inject.Singleton;
+import org.crac.Context;
+import org.crac.Resource;
 import org.h2.tools.Server;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,15 +17,43 @@ import java.sql.SQLException;
 
 @Requires(env = Environment.DEVELOPMENT)
 @Singleton
-public class H2Console implements ApplicationEventListener<StartupEvent> {
+public class H2Console implements ApplicationEventListener<StartupEvent>, OrderedResource {
     private static final Logger LOG = LoggerFactory.getLogger(H2Console.class);
+    private final Server webServer;
+    public H2Console() {
+        try {
+            webServer = Server.createWebServer();
+        } catch (SQLException e) {
+            throw new ConfigurationException("unable to create the H2 console");
+        }
+    }
+
     @Override
     public void onApplicationEvent(StartupEvent event) {
+        start();
+    }
+
+    void start() {
         try {
             LOG.info("starting h2 console in http://localhost:8082");
-            Server.createWebServer().start();
+            webServer.start();
         } catch (SQLException e) {
             LOG.warn("Unable to start H2 Console");
         }
+    }
+
+    void stop() {
+        LOG.info("stopping h2 console in http://localhost:8082");
+        webServer.stop();
+    }
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+        stop();
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+        start();
     }
 }
